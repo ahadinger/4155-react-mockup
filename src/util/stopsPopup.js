d1e27660ff9c05966f0ceb988ca4a46f00ff9262
ahadinger <=>
2022-11-03 16:04:03 -0400

    revamped stops popup  to show if a stop is in more than one route

--- src/util/stopsPopup.js
+++ src/util/stopsPopup.js
@@ -5,28 +5,28 @@ import '../PopUp.css';
 //need to get the data in the future
 
 export const showStopPopup = (stop) => {
-    const n = stop.route;
+    const n = stop.route[0];
+    const rows = [];
+    for(let i = 0; i < stop.route.length; i++){
+        rows.push(
+            <>
+            <Row className="Card-cell">
+                <Col className="Card-bold">Route</Col>
+                <Col className={stop.route[i].replace(' ', '-')} style={{ textAlign: 'right' }}>{stop.route[i]}</Col>
+            </Row>
+            <Row className="Card-cell">
+                <Col className="Card-bold">Next Bus</Col>
+                <Col style={{ textAlign: 'right' }}>5 minutes</Col>
+            </Row>
+            </>
+        )
+    }
     return (
     <Container>
         <Row>
             <Col className="Card-title"><h5>{stop.name}</h5></Col>
         </Row>
-        <Row className="Card-cell">
-            <Col className="Card-bold">Route</Col>
-            <Col className={n.replace(' ', '-')} style={{ textAlign: 'right' }}>{stop.route}</Col>
-        </Row>
-        <Row className="Card-cell">
-            <Col className="Card-bold">Next Bus</Col>
-            <Col style={{ textAlign: 'right' }}>5 minutes</Col>
-        </Row>
-        <Row className="Card-cell">
-            <Col className="Card-bold">Something</Col>
-            <Col style={{ textAlign: 'right' }}>Something else</Col>
-        </Row>
-        <Row className="Card-cell">
-            <Col className="Card-bold">Something</Col>
-            <Col style={{ textAlign: 'right' }}>Something else</Col>
-        </Row>
+        {rows}
     </Container>
     )
 }
